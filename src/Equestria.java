9be3f580b6d1794f98b786f22ff588debfa4ec66
Ethan <Ethan@10.0.0.2>
2019-10-28 17:49:57 -0700

    Done with Exercise 1

--- src/Equestria.java
+++ src/Equestria.java
@@ -3,7 +3,15 @@ public class Equestria {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//not started
+		double BM = distance(31, 15, 35, 9);		
+		System.out.println("Distance between Baltimare and Manehattan: " + BM);
+		double LPNF = distance(6, 20, 24, 7);
+		System.out.println("Distance between Los Pegasus and Neighagra Falls: " + LPNF);
+		double BP = distance(28, 22, 16, 13);
+		System.out.println("Distance between Badlands and Ponyville: " + BP);
 	}
-
+	//this method uses formula to find distance between cities
+	public static double distance(int x1, int y1, int x2, int y2) {
+		return Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+	}
 }
